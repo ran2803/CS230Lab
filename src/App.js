8ff8e4337519fdaef15859d404513e3a41a05f77
ran2803 <ran00003@mix.wvu.edu>
2023-10-30 20:52:56 -0400

    navbar and boxes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/navbar';
+import './components/cards';
+import NavBar from './components/navbar';
+
 
 function App() {
   return (
+  <>
     <div className="App">
+      <NavBar/>
       <h1>CS 230L</h1>
         <h2>Section - 002</h2>
         <p>WVU ID: 800367486</p>
         <p>Hi I am Robert Needham</p>
     </div>
+    <div>
+      <div className="Cards">
+      <Cards/>
+      <Cards/>
+      <Cards/>
+      </div>
+    </div>
+  </>
   );
 }
 
